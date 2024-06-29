4b898f9e249ee04bf857e6e5d2c89765258f9405
antonin.kriz <antonin.kriz@gmail.com>
2024-06-29 19:37:49 +0200

    disabled msvc deprecated warnings

--- cmake/spb_warnings.cmake
+++ cmake/spb_warnings.cmake
@@ -1,6 +1,7 @@
 function(spb_enable_warnings TARGET)
     if(MSVC)
         target_compile_options(${TARGET} PRIVATE /W4 /WX)
+        target_compile_definitions(${TARGET} PRIVATE _CRT_SECURE_NO_WARNINGS)
     else()
         target_compile_options(${TARGET} PRIVATE -Wall -Wextra -Wpedantic -Werror -Wno-missing-field-initializers)
     endif()
