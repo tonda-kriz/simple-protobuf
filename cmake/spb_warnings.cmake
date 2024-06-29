eb1a30cd4256f069d3f4b2d0a84feb8361794b5e
antonin.kriz <antonin.kriz@gmail.com>
2024-06-29 22:21:05 +0200

    disabled int64 to int conversion

--- cmake/spb_warnings.cmake
+++ cmake/spb_warnings.cmake
@@ -1,7 +1,7 @@
 function(spb_enable_warnings TARGET)
     if(MSVC)
         # https://developercommunity.visualstudio.com/t/c2017-illegal-escape-sequence-when-using-in-a-raw/919371
-        target_compile_options(${TARGET} PRIVATE /W4 /WX /wd4996 /Zc:preprocessor)
+        target_compile_options(${TARGET} PRIVATE /W4 /WX /wd4996 /wd2220 /Zc:preprocessor)
         target_compile_definitions(${TARGET} PRIVATE _CRT_SECURE_NO_WARNINGS)
     else()
         target_compile_options(${TARGET} PRIVATE -Wall -Wextra -Wpedantic -Werror -Wno-missing-field-initializers)
