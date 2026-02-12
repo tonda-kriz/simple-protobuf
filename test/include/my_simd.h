f2109fc9f6e934dd0497f490c5806fba57a96a54
Antonin Kriz <antonin.kriz@gmail.com>
2026-02-12 17:34:09 +0100

    fixup! fixed simd include

--- test/include/my_simd.h
+++ test/include/my_simd.h
@@ -4,10 +4,6 @@
 #include <simd>
 template < class Tp, int Np >
 using my_fixed_size_simd = std::fixed_size_simd< Tp, Np >;
-#elif __has_include( <experimental/simd>)
-#include <experimental/simd>
-template < class Tp, int Np >
-using my_fixed_size_simd = std::experimental::fixed_size_simd< Tp, Np >;
 #else
 //- no simd, use fixed-size-array instead to satisfy the unit tests
 #include <array>
